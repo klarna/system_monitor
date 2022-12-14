98a58b5f935b34b231e3e46536006aeb8c782532
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2022-12-14 12:01:03 +0200

    Add tests

--- test/system_monitor_tests.erl
+++ test/system_monitor_tests.erl
@@ -5,3 +5,9 @@
 start_test() ->
   ?assertMatch({ok, _}, application:ensure_all_started(system_monitor)),
   application:stop(system_monitor).
+
+restart_pg_test() ->
+  application:set_env(system_monitor, callback_mod, system_monitor_pg),
+  ?assertMatch({ok, _}, application:ensure_all_started(system_monitor)),
+  ok = system_monitor_pg:stop(),
+  ok = system_monitor_pg:start().
